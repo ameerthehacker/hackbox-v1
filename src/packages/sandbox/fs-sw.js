5e9a63f425245e07981057d851b4ceaabb05a3b3
Ameer Jhan <ameerjhanprof@gmail.com>
2019-12-27 16:23:32 +0530

    feat(sandbox): setup a minimalistic file server in sw

--- src/packages/sandbox/fs-sw.js
+++ src/packages/sandbox/fs-sw.js
@@ -1,5 +1,37 @@
-self.addEventListener('activate', (evt) => evt.skipWaiting());
+const FS_PREFIX = '/fs/';
+let files = {};
+
+self.addEventListener('install', () => self.skipWaiting());
 
 self.addEventListener('fetch', (evt) => {
-  console.log(evt);
+  let requestUrl = evt.request.url;
+  let fsPrefixIndex = requestUrl.search(FS_PREFIX);
+
+  if (fsPrefixIndex !== -1) {
+    let fsPath = requestUrl.substring(fsPrefixIndex + FS_PREFIX.length);
+
+    if (files[fsPath] === undefined) {
+      evt.respondWith(
+        new Response(null, {
+          status: 404
+        })
+      );
+
+      return;
+    }
+
+    evt.respondWith(
+      new Response(files[fsPath], {
+        ContentType: 'application/javascript'
+      })
+    );
+  }
+});
+
+self.addEventListener('message', (evt) => {
+  let { type, content } = evt.data;
+
+  if (type === 'files') {
+    files = content;
+  }
 });
--- src/packages/sandbox/src/app.js
+++ src/packages/sandbox/src/app.js
@@ -1,5 +1,33 @@
 import * as fsSW from './fs-sw';
 
+/* eslint-disable no-undef */
+System.config({
+  baseURL: '/fs',
+  map: {
+    traceur: 'https://cdn.jsdelivr.net/npm/traceur@0.0.111/bin/traceur.js'
+  }
+});
+
+System.import('main.js');
+
+const files = {
+  'main.js': `
+    import hello from './modules/hello'
+
+    hello();
+  `,
+  'modules/hello': `
+    export default function hello() {
+      console.log('hello world from sandbox');
+    }
+  `
+};
+
 // register the file system service worker
-fsSW.register();
+fsSW.register().then(() => {
+  navigator.serviceWorker.controller.postMessage({
+    type: 'files',
+    content: files
+  });
+});
 document.writeln('Hello world from sandbox!');
