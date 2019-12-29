ccdf92837c000521e6e212498bdb6eae3940180a
Ameer Jhan <ameerjhanprof@gmail.com>
2019-12-29 13:33:14 +0530

    feat(files): design basic file explorer

---
+++ src/packages/client/src/components/editor/components/file-explorer/file-explorer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function FileExplorer() {
+  return <span>File explorer</span>;
+}
+
+export default FileExplorer;
--- src/packages/client/src/components/editor/editor.js
+++ src/packages/client/src/components/editor/editor.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import MonacoEditor from '@monaco-editor/react';
+import FileExplorer from './components/file-explorer';
+import { Grid } from '@material-ui/core';
 
 function Editor() {
-  return <MonacoEditor theme="vs-dark" height="100vh" language="javascript" />;
+  return (
+    <Grid container>
+      <Grid item xs={2}>
+        <FileExplorer />
+      </Grid>
+      <Grid item xs={10}>
+        <MonacoEditor theme="vs-dark" height="100vh" language="javascript" />
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Editor;
