52bc7137f93d4c9362d367ea93c23058d1603aad
Ameer Jhan <ameerjhanprof@gmail.com>
2019-12-29 13:19:05 +0530

    feat(client): add a basic monaco editor

---
+++ src/packages/client/src/components/editor/editor.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MonacoEditor from '@monaco-editor/react';
+
+function Editor() {
+  return <MonacoEditor theme="vs-dark" height="100vh" language="javascript" />;
+}
+
+export default Editor;
--- src/packages/client/src/pages/app/app.js
+++ src/packages/client/src/pages/app/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import Editor from '../../components/editor';
 
 export default function App() {
-  return <h1>Hello World from hackbox client</h1>;
+  return <Editor />;
 }
